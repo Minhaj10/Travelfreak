3d6c95e942136a5c065a6ae01ff283e0ecd6ece2
Minhaj ahmed <minhajumid987@gmail.com>
2021-02-17 13:21:09 +0600

    routing,backend folder structure,controllers,jsx structure,styling,installing redux

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(bodyParser.json({limit: "30mb",extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb",extended: true}));
 app.use(cors());
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL ='mongodb+srv://minhaj_10:p300whynot400@cluster0.7foa5.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
