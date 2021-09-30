efbc245967dc530fc24d0f82a3fc4ceecdc29437
Minhaj10 <minhajumid987@gmail.com>
2021-10-01 00:20:31 +0600

    pagination 10%

---
+++ Travel freak/client/src/components/Pagination.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import {Pagination ,PaginationItem} from '@material-ui/lab'
+import {Link} from 'react-router-dom'
+import styles from './styles'
+
+const Paginate=()=>{
+    const classes =useStyles();
+
+    return(
+        <Pagination classes={{ul : classes.ul}} count ={5} page={1} variant="outlined" color="primary" 
+        renderItem={(item)=>(
+            <PaginationItem {...item} component={Link}/>
+        )}
+        />
+    )
+}
+
