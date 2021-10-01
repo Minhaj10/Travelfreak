7a837cce35a4e24c6d4b72415d0760a8d0406971
Minhaj10 <minhajumid987@gmail.com>
2021-10-01 17:27:04 +0600

    pagination & route settings

--- Travel freak/client/src/components/Home/Home.js
+++ Travel freak/client/src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid ,Paper} from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-
+import  Pagination  from '../Pagination.jsx';
 import { getPosts } from '../../actions/posts';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
@@ -23,6 +23,9 @@ const Home = () => {
           </Grid>
           <Grid item xs={12} sm={4}>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper  elevation={6}>
+                <Pagination/>
+            </Paper>
           </Grid>
         </Grid>
       </Container>
