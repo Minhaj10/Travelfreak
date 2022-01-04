eb96d69d0dee205cc821084c90081f832f7b6b59
Minhaj10 <minhajumid987@gmail.com>
2022-01-04 18:43:33 +0600

    update version 2.0

--- Travel freak/client/src/components/Home/Home.js
+++ Travel freak/client/src/components/Home/Home.js
@@ -1,80 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid ,Paper, AppBar,Button,TextField} from '@material-ui/core';
+import React, { useState } from 'react';
+import { Container, Grow, Grid, AppBar, TextField, Button, Paper } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-import  Pagination  from '../Pagination.jsx';
-import { getPosts,getPostsBySearch } from '../../actions/posts';
-import Posts from '../Posts/Posts';
-import Form from '../Form/Form';
-import { useHistory,useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import ChipInput from 'material-ui-chip-input';
 
-import useStyles from './styles'
+import { getPostsBySearch } from '../../actions/posts';
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
+import Pagination from '../Pagination';
+import useStyles from './styles';
 
-function useQuery(){
-  return new URLSearchParams(useLocation().search)
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
 }
 const Home = () => {
+  const classes = useStyles();
+  const query = useQuery();
+  const page = query.get('page') || 1;
+  const searchQuery = query.get('searchQuery');
+
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
-  const query =useQuery();
-  const history =useHistory();
-  const page = query.get('page') || 1;
-  const searchQuery= query.get('searchQuery');
-  const classes =useStyles();
-  const [search, setsearch] = useState('');
-  const [tags, settags] = useState([]);
 
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
+  const [search, setSearch] = useState('');
+  const [tags, setTags] = useState([]);
+  const history = useHistory();
 
-  const searchPost=()=>{
-    if(search.trim()){
-      dispatch(getPostsBySearch({ search ,tags:tags.join(',')}))
-    }
-    else{
+  const searchPost = () => {
+    if (search.trim() || tags) {
+      dispatch(getPostsBySearch({ search, tags: tags.join(',') }));
+      history.push(`/posts/search?searchQuery=${search || 'none'}&tags=${tags.join(',')}`);
+    } else {
       history.push('/');
     }
-  }
-  const handleKeyPress=(e)=>{
-    //keycode 13 means -> enter key
-        if(e.KeyCode===13){
-            searchPost();
-        }
-  }
+  };
 
-  const handleAdd=(tag)=> settags([...tags,tag])
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      searchPost();
+    }
+  };
 
-  const handleDelete=(tagToDelete)=> settags(tags.filter((tag)=> tag!= tagToDelete));
+  const handleAddChip = (tag) => setTags([...tags, tag]);
+
+  const handleDeleteChip = (chipToDelete) => setTags(tags.filter((tag) => tag !== chipToDelete));
 
   return (
     <Grow in>
       <Container maxWidth="xl">
-        <Grid container justify="space-between" alignItems="stretch" spacing={3} className={classes.gridcontainer}>
+        <Grid container justify="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
           <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <AppBar className={classes.appBarSearch} position ="static" color="inherit">
-                <TextField name ="search" variant="outlined" label ="Search for places"
-                fullWidth
-                onKeyPress={handleKeyPress}
-                value={search}
-                onChange={(e)=>{setsearch(e.target.value)}}/>
-                <ChipInput
-                style={{margin :'10px 0'}}
+            <AppBar className={classes.appBarSearch} position="static" color="inherit">
+              <TextField onKeyDown={handleKeyPress} name="search" variant="outlined" label="Search Posts" fullWidth value={search} onChange={(e) => setSearch(e.target.value)} />
+              <ChipInput
+                style={{ margin: '10px 0' }}
                 value={tags}
-                onAdd={handleAdd}
-                onDelete={handleDelete}
-                label="Search by tags"
+                onAdd={(chip) => handleAddChip(chip)}
+                onDelete={(chip) => handleDeleteChip(chip)}
+                label="Search Tags"
                 variant="outlined"
-                />
-                <button onClick={searchPost} className={classes.searchButton} color="primary">Search</button>
+              />
+              <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
             </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
-            <Paper  elevation={6}>
-                <Pagination/>
-            </Paper>
+            {(!searchQuery && !tags.length) && (
+              <Paper className={classes.pagination} elevation={6}>
+                <Pagination page={page} />
+              </Paper>
+            )}
           </Grid>
         </Grid>
       </Container>
--- Travel freak/client/src/components/styles.js
+++ Travel freak/client/src/components/styles.js
@@ -1,8 +1,7 @@
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(()=>({
-    ul:{
-        justifyContent: 'space-round',
-    },
+export default makeStyles(() => ({
+  ul: {
+    justifyContent: 'space-around',
+  },
 }));
-
